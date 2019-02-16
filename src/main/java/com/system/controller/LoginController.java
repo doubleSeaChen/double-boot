90e90ac2643c2b4ca5a441ec6f41a2a757dd37e6
doubleSeaChen <1664544330@qq.com>
2019-02-16 13:15:44 +0800

    登录控制

--- src/main/java/com/system/controller/LoginController.java
+++ src/main/java/com/system/controller/LoginController.java
@@ -13,6 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * 登录控制
+ */
 @Controller
 public class LoginController {
 
